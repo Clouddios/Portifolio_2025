c21965b86950050b4bbdb766dc884f418d801a61
Claudio Almeida <claudio132009@gmail.com>
2025-06-07 09:01:06 -0300

    Correção da importação de imagens Pagina de projeto

--- src/Pages/Home/sections/ProjectsSection/ProjectsSection.tsx
+++ src/Pages/Home/sections/ProjectsSection/ProjectsSection.tsx
@@ -1,6 +1,11 @@
 import { Box, Container, Grid, Typography, styled } from "@mui/material";
 import ProjectCard, { ProjectCardProps } from "../../../../components/ProjectCard/ProjectCard";
 import AnimationComponent from "../../../../components/AnimationComponent/AnimationComponent";
+import recFacialImg from '../../../../assets/images/rec_facial.png';
+import capitechImg from '../../../../assets/images/capitech.png';
+import zipUrlImg from '../../../../assets/images/projeto_zipurl.png';
+import arduinoImg from '../../../../assets/images/arduino.jpeg';
+
 
 const ProjectsSection: React.FC = () => {
 
@@ -10,9 +15,27 @@ const ProjectsSection: React.FC = () => {
 
     const projects: ProjectCardProps[] = [
         {
+            title: "Capitech – Plataforma Educacional ",
+            subtitle: "2025",
+            srcImg: capitechImg,
+            description: "O projeto capitech-mobile é a versão mobile do sistema Capitech, voltado para educação tecnológica, com foco em fornecer ensino gratuito e de qualidade por meio de dispositivos móveis.",
+            technologies: "Technologies: React, TypeScript, Tailwind, Yup, Axios",
+            websiteURL: "https://capitech-front-final.vercel.app/",
+            codeURL: "https://github.com/CapitechDev/capitech-mobile",
+        },
+        {
+            title: "ZipURL – encurtador de URLs",
+            subtitle: "Jan 2024 - Mar 2024",
+            srcImg: zipUrlImg, // você pode trocar isso por um ícone/imagem representativa
+            description: "Aplicação completa para encurtamento de URLs, com back-end em Node.js e front-end em React. Permite criar URLs curtas, redirecionamento automático e visual limpo.",
+            technologies: "Technologies: Node.js, Express, MongoDB, React, Vercel",
+            websiteURL: "https://zipurl-gray.vercel.app/",
+            codeURL: "https://github.com/Clouddios/encurtadorBackEnd",
+        },
+        {
             title: "Sistema de Reconhecimento Facial",
             subtitle: "2023 - Presente",
-            srcImg: "/src/assets/images/rec_facial.png",
+            srcImg: recFacialImg,
             description: "Sistema acadêmico que compara diferentes algoritmos de reconhecimento facial (como HOG e CNN), testando precisão e performance. Inclui geração de relatórios e testes práticos com imagens.",
             technologies: "Technologies: Python, face_recognition, Jupyter, OpenCV",
             websiteURL: "https://github.com/seu-usuario/projeto-reconhecimento-facial",
@@ -21,30 +44,13 @@ const ProjectsSection: React.FC = () => {
         {
             title: "Sistema IoT de Monitoramento de Temperatura",
             subtitle: "2023",
-            srcImg: "/src/assets/images/arduino.jpeg",
+            srcImg: arduinoImg,
             description: "Projeto para monitoramento remoto de temperatura e umidade em geladeira de medicamentos, com sensores DHT11/DHT22, ESP8266 e envio dos dados via API para banco de dados.",
             technologies: "Technologies: Arduino, C++, ESP8266, DHT11, API REST, Node.js",
             websiteURL: "https://github.com/seu-usuario/projeto-iot-geladeira",
             codeURL: "https://github.com/seu-usuario/projeto-iot-geladeira",
-        },
-        {
-            title: "ZipURL – encurtador de URLs",
-            subtitle: "Jan 2024 - Mar 2024",
-            srcImg: "/src/assets/images/projeto_zipurl.png", // você pode trocar isso por um ícone/imagem representativa
-            description: "Aplicação completa para encurtamento de URLs, com back-end em Node.js e front-end em React. Permite criar URLs curtas, redirecionamento automático e visual limpo.",
-            technologies: "Technologies: Node.js, Express, MongoDB, React, Vercel",
-            websiteURL: "https://zipurl-gray.vercel.app/",
-            codeURL: "https://github.com/Clouddios/encurtadorBackEnd",
-        },
-        {
-            title: "Capitech – Plataforma Educacional ",
-            subtitle: "2025",
-            srcImg: "/src/assets/images/capitech.png",
-            description: "O projeto capitech-mobile é a versão mobile do sistema Capitech, voltado para educação tecnológica, com foco em fornecer ensino gratuito e de qualidade por meio de dispositivos móveis.",
-            technologies: "Technologies: React, TypeScript, Tailwind, Yup, Axios",
-            websiteURL: "https://capitech-front-final.vercel.app/",
-            codeURL: "https://github.com/CapitechDev/capitech-mobile",
         }
+
     ];
 
     return (
